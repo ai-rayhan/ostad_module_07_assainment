7db8a2b76189a876a275be5511b4f389aa5861dd
Rayhan Ahmed Imran <114678962+ai-rayhan@users.noreply.github.com>
2023-10-17 03:22:01 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ostad_module_07_assainment/models/product.dart';
-import 'screens/cart_screen.dart';
-import 'widgets/product_item.dart';
+import '/views/list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,52 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Product Catalog',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ProductList(),
+      home: TextListScreen(),
     );
   }
 }
-
-class ProductList extends StatefulWidget {
-  const ProductList({super.key});
-
-  @override
-  ProductListState createState() => ProductListState();
-}
-
-class ProductListState extends State<ProductList> {
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product List'),
-      ),
-      floatingActionButton: FloatingActionButton( 
-        child: const Icon(Icons.shopping_cart_rounded),
-         onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CartScreen(products: products),
-                ),
-              );
-            },),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          return ProductItem(
-            product: products[index],
-          );
-        },
-      ),
-    );
-  }
-}
-
